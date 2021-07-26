6e51a53fbfb505330607ae8dca4245a678eed86f
Jasmin Grbo <46293362+grb0@users.noreply.github.com>
2021-07-26 08:57:10 +0200

    Set up splash screen

--- app/src/main/java/ba/grbo/weatherchecker/WeatherCheckerActivity.kt
+++ app/src/main/java/ba/grbo/weatherchecker/WeatherCheckerActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 
 class WeatherCheckerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_WeatherChecker)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_weather_checker)
     }
