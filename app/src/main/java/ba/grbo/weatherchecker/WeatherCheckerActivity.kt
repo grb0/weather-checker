0acd7b0fb545f26825f1e44d592a05c09cbb5952
Jasmin Grbo <46293362+grb0@users.noreply.github.com>
2021-07-26 07:32:43 +0200

    Init commit

---
+++ app/src/main/java/ba/grbo/weatherchecker/WeatherCheckerActivity.kt
@@ -0,0 +1,11 @@
+package ba.grbo.weatherchecker
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class WeatherCheckerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_weather_checker)
+    }
+}
