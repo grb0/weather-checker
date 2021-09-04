88f99f84921cc614afaff5188d9a4f381e15312f
Jasmin Grbo <46293362+grb0@users.noreply.github.com>
2021-09-04 17:14:08 +0200

    Refactor code

--- app/src/main/java/ba/grbo/weatherchecker/util/Tree.kt
+++ app/src/main/java/ba/grbo/weatherchecker/util/Tree.kt
@@ -2,12 +2,16 @@ package ba.grbo.weatherchecker.util
 
 import android.util.Log
 import ba.grbo.weatherchecker.util.Constants.COMMON_TAG
+import ba.grbo.weatherchecker.util.Tree.Pattern.*
 import com.google.firebase.crashlytics.FirebaseCrashlytics
 import timber.log.Timber
 import java.util.concurrent.atomic.AtomicInteger
 
 abstract class Tree private constructor() : Timber.DebugTree() {
     private lateinit var element: StackTraceElement
+    private val debugMessagePattern = "message: %s\n  info:    %s\n  thread:  %s\n "
+    protected open val errorMessagePattern =
+        "exception:  %s\n  message:    %s\n  info:       %s\n  thread:     %s\n  suppressed: %s\n "
 
     protected val StackTraceElement.info: String
         get() = String.format(
@@ -25,13 +29,53 @@ abstract class Tree private constructor() : Timber.DebugTree() {
             return stackTrace[index + 1]
         }
 
-    protected val Throwable.throwableMessage: String
-        get() = "${stackTrace[0].info} - ${this::class.simpleName}"
+    private val Throwable?.simpleClassNameOrNone: String
+        get() = if (this != null) this::class.simpleName ?: "None" else "None"
 
-    protected abstract val Throwable.throwableMessages: String
+    private val Throwable?.messageOrNone: String
+        get() = this?.message ?: "None"
+
+    protected open fun getSuppressedPattern(
+        position: Int,
+        lastPosition: Int
+    ): String = when (position) {
+        0 -> FIRST.value
+        lastPosition -> LAST.value
+        else -> MIDDLE.value
+    }
+
+    protected open val Throwable?.suppressedInfoOrNone: String
+        get() {
+            return if (this != null) {
+                if (suppressed.isEmpty()) "None"
+                else suppressed.run {
+                    val sB = StringBuilder()
+                    forEachIndexed { i, t ->
+                        sB.append(
+                            String.format(
+                                getSuppressedPattern(i, lastIndex),
+                                i + 1,
+                                t.simpleClassNameOrNone,
+                                t.messageOrNone,
+                                t.stackTrace[0].info
+                            )
+                        )
+
+                    }
+                    sB.toString()
+                }
+            } else "None"
+        }
+
+    private enum class Pattern(val value: String) {
+        FIRST("%d. exception: %s\n                 message: %s\n                 info: %s\n"),
+        LAST("              %d. exception: %s\n                 message: %s\n                 info: %s"),
+        MIDDLE("              %d. exception: %s\n                 message: %s\n                 info: %s\n"),
+        UNSPACED("%d. exception: %s message: %s info: %s")
+    }
 
     override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
-        super.log(priority, tag, createModifiedMessage(message, priority), t)
+        super.log(priority, tag, createModifiedMessage(message, t, priority), t)
     }
 
     override fun createStackElementTag(element: StackTraceElement): String {
@@ -40,19 +84,27 @@ abstract class Tree private constructor() : Timber.DebugTree() {
         return COMMON_TAG
     }
 
+    open fun createDebugMessage(message: String) = String.format(
+        debugMessagePattern,
+        message,
+        element.info,
+        Thread.currentThread().name
+    )
+
+    open fun createErrorMessage(throwable: Throwable?) = String.format(
+        errorMessagePattern,
+        throwable.simpleClassNameOrNone,
+        throwable.messageOrNone,
+        element.info,
+        Thread.currentThread().name,
+        throwable.suppressedInfoOrNone
+    )
+
     open fun createModifiedMessage(
         message: String,
+        throwable: Throwable?,
         priority: Int
-    ) = if (priority == Log.DEBUG) String.format(
-        "%s - %s - %s",
-        Thread.currentThread().name,
-        element.info,
-        message
-    ) else String.format(
-        "%s - %s",
-        Thread.currentThread().name,
-        message
-    )
+    ) = if (priority == Log.DEBUG) createDebugMessage(message) else createErrorMessage(throwable)
 
     companion object {
         val Timber.Forest.DEBUG_TREE: Tree
@@ -65,44 +117,27 @@ abstract class Tree private constructor() : Timber.DebugTree() {
             object : Tree() {
                 private var counter = AtomicInteger(1)
 
-                override val Throwable.throwableMessages: String
-                    get() {
-                        val sB = StringBuilder("$throwableMessage\n  suppressed:")
-                        suppressed.forEachIndexed { i, t ->
-                            sB.append("\n             ${i + 1}. ${t.throwableMessage}")
-                        }
-                        return sB.toString()
-                    }
-
-                override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
-                    if (priority == Log.ERROR && t != null) {
-                        // Passing null as throwable since we don't want to pollute the message with
-                        // its stackTrace
-                        super.log(priority, tag, t.throwableMessages, null)
-                    } else super.log(priority, tag, message, t)
-                }
-
                 override fun createModifiedMessage(
                     message: String,
+                    throwable: Throwable?,
                     priority: Int
                 ) = String.format(
                     "%d. %s",
                     counter.getAndIncrement(),
-                    super.createModifiedMessage(message, priority)
+                    super.createModifiedMessage(message, throwable, priority)
                 )
             }
         }
 
         private val RELEASE: Tree by lazy {
             object : Tree() {
-                override val Throwable.throwableMessages: String
-                    get() {
-                        val sB = StringBuilder("$throwableMessage | suppressed:")
-                        suppressed.forEachIndexed { i, t ->
-                            sB.append(" ${i + 1}. ${t.throwableMessage} |")
-                        }
-                        return sB.toString()
-                    }
+                override val errorMessagePattern =
+                    "exception: %s, message: %s, info: %s, thread: %s, suppressed: %s"
+
+                override fun getSuppressedPattern(
+                    position: Int,
+                    lastPosition: Int
+                ): String = UNSPACED.value
 
                 override fun log(
                     priority: Int,
@@ -114,7 +149,7 @@ abstract class Tree private constructor() : Timber.DebugTree() {
                         FirebaseCrashlytics.getInstance().run {
                             // Since recordException is not taking care of possible suppressed
                             // exceptions, we log those manually
-                            log(createModifiedMessage(t.throwableMessages, priority))
+                            log(createModifiedMessage(message, t, priority))
                             recordException(t)
                         }
                     }
